68a01961715ebd763122df5619532a0e5648166e
Oleg Loutchansky <luchansodev@gmail.com>
2020-09-13 16:39:26 +0300

    fix(*): rename functions

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@ import "./App.css";
 import {
   isLess11MinutesSelector,
   gpmSelector,
-  benchmarksGPM95Selector,
-  benchmarksLVL95Selector,
+  benchmarksGPMSelector,
+  benchmarksLVLSelector,
   LHSelector,
   LHTENSelector,
 } from "./state/state-selector";
+import { RoshanButton } from "./components/RoshanButton";
+import { Draggable } from "./components/Draggable";
 
 function App() {
   const state = useDota2();
@@ -17,14 +19,16 @@ function App() {
 
   return (
     <div className="App">
+      <Draggable />
       <header className="App-header">
         {/* <Stat name="time" value={clockTimeSelector(state)} /> */}
         <Stat name="GPM" value={gpmSelector(state)} />
-        ----- 95% -----
-        <Stat name="GPM" value={benchmarksGPM95Selector(state)} />
-        <Stat name="LVL" value={benchmarksLVL95Selector(state)} />
+        ----- 99% -----
+        <Stat name="GPM" value={benchmarksGPMSelector(state)} />
+        <Stat name="LVL" value={benchmarksLVLSelector(state)} />
         <Stat name="LH" value={LHSelector(state)} />
         {isLess11Minutes && <Stat name="LHTEN" value={LHTENSelector(state)} />}
+        <RoshanButton state={state} />
       </header>
     </div>
   );
