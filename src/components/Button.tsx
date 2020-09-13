8bc1391eacd6bd1305e34346164a7fd543c7ea50
Oleg Loutchansky <luchansodev@gmail.com>
2020-09-13 16:39:48 +0300

    feat(ui): add button

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+export const Button = styled.button`
+  background-color: rgba(40, 44, 52, 0.8);
+  padding: 5px;
+  border: none;
+  color: white;
+  cursor: pointer;
+  outline: none;
+`;
