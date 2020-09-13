c649663a469a3e348a0167cfb8c237735efb77ba
Oleg Loutchansky <luchansodev@gmail.com>
2020-09-13 17:08:42 +0300

    feat(ui): add roshan timer

--- src/components/RoshanButton/index.tsx
+++ src/components/RoshanButton/index.tsx
@@ -1,100 +1,32 @@
-import React, { memo, useState } from "react";
+import React, { memo } from "react";
 import { State } from "../../state/state";
-import { gameTimeSelector } from "../../state/state-selector";
 import { Button } from "../Button";
 import { Stat } from "../Stat/Stat";
+import { useRoshanSpawn } from "../../hooks/useRoshanSpawn/useRoshanSpawn";
 
 type Props = {
   state: State;
 };
 
-type RoshanStopwatch = {
-  time: number;
-  isActive: boolean;
-};
-
-const RESPAWN_TIME = {
-  MINIMAL: 8 * 60,
-  MAXIMUM: 11 * 60,
-};
-
-function normalizeNumber(num: number) {
-  if (num < 10) {
-    return `0${num}`;
-  }
-
-  return num.toString();
-}
-
-function formatSeconds(time: number): string {
-  const minutes = Math.floor(time / 60);
-  const seconds = time % 60;
-
-  return `${normalizeNumber(minutes)}:${normalizeNumber(seconds)}`;
-}
-
-function roshanIsDead(
-  { isActive, time }: RoshanStopwatch,
-  currentGameTime: number
-) {
-  return isActive && time + RESPAWN_TIME.MINIMAL > currentGameTime;
-}
-
-function roshanTimeDeadSelector(
-  { time }: RoshanStopwatch,
-  currentGameTime: number
-) {
-  const timeSinceDead = currentGameTime - time;
-  const result = RESPAWN_TIME.MAXIMUM - timeSinceDead;
-
-  return formatSeconds(result);
-}
-
-function schrodingerRoshan(
-  { isActive, time }: RoshanStopwatch,
-  currentGameTime: number
-) {
-  return (
-    isActive &&
-    time + RESPAWN_TIME.MINIMAL < currentGameTime &&
-    time + RESPAWN_TIME.MAXIMUM > currentGameTime
-  );
-}
-
 export const RoshanButton = memo(function RoshanButton({ state }: Props) {
-  const currentGameTime = gameTimeSelector(state) || 0;
-  const [roshanStopwatch, setRoshanStopwatch] = useState<RoshanStopwatch>({
-    isActive: false,
-    time: 0,
-  });
+  const {
+    handleDead,
+    handleReset,
+    isDead,
+    isDeadOrLive,
+    timeToSpawn,
+  } = useRoshanSpawn(state);
 
-  function handleClick() {
-    setRoshanStopwatch({
-      time: currentGameTime,
-      isActive: true,
-    });
-  }
-
-  function handleReset() {
-    setRoshanStopwatch({
-      time: 0,
-      isActive: false,
-    });
-  }
-
-  if (
-    roshanIsDead(roshanStopwatch, currentGameTime) ||
-    schrodingerRoshan(roshanStopwatch, currentGameTime)
-  ) {
+  if (isDead || isDeadOrLive) {
     return (
       <Stat
         onClick={handleReset}
         name="ROSH"
-        value={roshanTimeDeadSelector(roshanStopwatch, currentGameTime)}
-        warning={schrodingerRoshan(roshanStopwatch, currentGameTime)}
+        value={timeToSpawn}
+        warning={isDeadOrLive}
       />
     );
   }
 
-  return <Button onClick={handleClick}>ROSH</Button>;
+  return <Button onClick={handleDead}>ROSH</Button>;
 });
---
+++ src/hooks/useRoshanSpawn/useRoshanSpawn.ts
@@ -0,0 +1,112 @@
+import { useEffect, useState } from "react";
+import useSound from "use-sound";
+import { State } from "../../state/state";
+import { gameTimeSelector } from "../../state/state-selector";
+import roshanRespawnMp3 from "./spawn.mp3";
+
+type RoshanStopwatch = {
+  time: number;
+  isActive: boolean;
+  isPlayedSound: boolean;
+};
+
+const RESPAWN_TIME = {
+  MINIMAL: 8 * 60,
+  MAXIMUM: 11 * 60,
+};
+
+function normalizeNumber(num: number) {
+  if (num < 10) {
+    return `0${num}`;
+  }
+
+  return num.toString();
+}
+
+function formatSeconds(time: number): string {
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  return `${normalizeNumber(minutes)}:${normalizeNumber(seconds)}`;
+}
+
+function roshanIsDead(
+  { isActive, time }: RoshanStopwatch,
+  currentGameTime: number
+) {
+  return isActive && time + RESPAWN_TIME.MINIMAL > currentGameTime;
+}
+
+function roshanTimeDeadSelector(
+  { time }: RoshanStopwatch,
+  currentGameTime: number
+) {
+  const timeSinceDead = currentGameTime - time;
+  const result = RESPAWN_TIME.MAXIMUM - timeSinceDead;
+
+  return formatSeconds(result);
+}
+
+function schrodingerRoshan(
+  { isActive, time }: RoshanStopwatch,
+  currentGameTime: number
+) {
+  return (
+    isActive &&
+    time + RESPAWN_TIME.MINIMAL < currentGameTime &&
+    time + RESPAWN_TIME.MAXIMUM > currentGameTime
+  );
+}
+
+function needToPlaySound(
+  { time, isPlayedSound, isActive }: RoshanStopwatch,
+  currentGameTime: number
+) {
+  const timeSinceDead = currentGameTime - time;
+  // 30 seconds for Leeway
+  const result = timeSinceDead - RESPAWN_TIME.MINIMAL + 30;
+
+  return isActive && !isPlayedSound && result > 0;
+}
+
+export function useRoshanSpawn(state: State) {
+  const currentGameTime = gameTimeSelector(state) || 0;
+  const [play] = useSound(roshanRespawnMp3, { volume: 0.25 });
+  const [roshanStopwatch, setRoshanStopwatch] = useState<RoshanStopwatch>({
+    isActive: false,
+    time: 0,
+    isPlayedSound: false,
+  });
+
+  function handleDead() {
+    setRoshanStopwatch({
+      time: currentGameTime,
+      isActive: true,
+      isPlayedSound: false,
+    });
+  }
+
+  function handleReset() {
+    setRoshanStopwatch({
+      time: 0,
+      isActive: false,
+      isPlayedSound: false,
+    });
+  }
+
+  const isDead = roshanIsDead(roshanStopwatch, currentGameTime);
+  const isDeadOrLive = schrodingerRoshan(roshanStopwatch, currentGameTime);
+  const timeToSpawn = roshanTimeDeadSelector(roshanStopwatch, currentGameTime);
+
+  useEffect(() => {
+    if (needToPlaySound(roshanStopwatch, currentGameTime)) {
+      play();
+      setRoshanStopwatch({
+        ...roshanStopwatch,
+        isPlayedSound: true,
+      });
+    }
+  }, [roshanStopwatch, currentGameTime, setRoshanStopwatch]);
+
+  return { handleDead, handleReset, isDead, isDeadOrLive, timeToSpawn };
+}
