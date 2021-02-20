4a5391be9290e6a2d87ccbaf392a63006b87d5fc
Oleg Loutchansky <luchansodev@gmail.com>
2021-02-20 04:38:02 +0300

    fix: remove comments

--- server/server-log-reader.js
+++ server/server-log-reader.js
@@ -1,10 +1,5 @@
 import fs from "fs";
 
-// const state = {
-//   steamIds: [],
-//   serverLogPath: "",
-// };
-
 const DEFAULT_FILE =
   "C:\\Program Files (x86)\\Steam\\steamapps\\common\\dota 2 beta\\game\\dota\\server_log.txt";
 
@@ -17,18 +12,6 @@ const getDotaIdsFromLine = (line) => {
   }
   return dotaIds;
 };
-
-// const onChange = (eventType, fileName) => {
-//   fs.readFile(fileName, (err, data) => updateState(data));
-// };
-
-// export const run = (
-//   serverLogPath = DEFAULT_FILE
-// ) => {
-//   state.serverLogPath = serverLogPath;
-//   onChange("init", state.serverLogPath);
-//   fs.watch(state.serverLogPath).addListener("change", onChange);
-// };
 
 const getState = () => {
   return new Promise((res) => {
@@ -43,19 +26,6 @@ const getState = () => {
   });
 };
 
-// const updateState = (rowData) => {
-//   const rowString = rowData.toString();
-//   const startIndex = rowString.lastIndexOf("Lobby");
-//   const finishIndex = rowString.indexOf("\n", startIndex);
-//   const lobbyString = rowString.slice(startIndex, finishIndex);
-
-//   state.steamIds = getDotaIdsFromLine(lobbyString);
-// };
-
 export const getSteamIDs = () => {
   return getState();
 };
-
-// if (process.env.DEBUG_MODULE) {
-//   run();
-// }
