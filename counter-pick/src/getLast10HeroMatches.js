6b9544fbd7f26db82fabb17123a3a9f530620293
Oleg Loutchansky <luchansodev@gmail.com>
2021-12-15 13:40:51 +0300

    feat: add lastGameDate property

--- counter-pick/src/getLast10HeroMatches.js
+++ counter-pick/src/getLast10HeroMatches.js
@@ -1,10 +1,14 @@
 import { heroes } from "dotaconstants";
 
 export const getLast10HeroMatches = (matches) => {
-  return matches.reduce((prev, { hero_id }, index) => {
+  const games = matches.reduce((prev, { hero_id }, index) => {
     if (index > 10) {
       return prev;
     }
     return [...prev, heroes[hero_id].localized_name];
   }, []);
+  return {
+    games,
+    lastGameDate: new Date(matches[0].start_time * 1000).toLocaleDateString(),
+  };
 };
